ad5fde80dfc1dff831af565b6969bc112b213233
Anderson Guanche <anderson7guanche@gmail.com>
2025-10-12 09:43:59 +0200

    Subiendo carpeta con primer ejercicio

---
+++ ejercicios_JAVA/ejercicio_campana/Readme.md
@@ -0,0 +1,36 @@
+# 4.🔔 Dar la campanada
+
+## Descripción
+
+Crear un programa que lea del teclado dos valores que indiquen la **hora de comienzo** y la **hora de fin** (por ejemplo: `16:30` y `19:17`), y que muestre por pantalla la **cantidad de veces que un reloj emite campanadas**.
+
+Las campanadas se producen cuando el reloj marca los minutos: **0, 15, 30 y 45**.
+
+Se supone que:
+
+- La hora de comienzo es menor que la hora de fin.
+- Ambas horas pertenecen al mismo día.
+
+
+## Ejemplos
+
+**Ejemplo 1:**
+
+Hora de comienzo: 9:44
+
+Hora de fin: 10:01
+
+➡️ Resultado: **2 campanadas**
+
+**Ejemplo 2:**
+
+Hora de comienzo: 8:00
+
+Hora de fin: 10:07
+
+➡️ Resultado: **8 campanadas**
+
+
+## Objetivo
+
+Determinar cuántas veces suenan las campanas entre ambas horas, **incluyendo los límites** si corresponden.
---
+++ ejercicios_JAVA/ejercicio_campana/src/main/java/DarLaCampanada/Main.java
@@ -0,0 +1,117 @@
+package DarLaCampanada;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Necesitamos Scanner para pedir datos al usuario
+        Scanner scn = new Scanner(System.in);
+
+        //Creamos 4 variables para guardar los tiempos
+        int horaInicio = 0;
+        int horaFin = 0;
+        int minutoInicio = 0;
+        int minutoFin = 0;
+        int minMediNoInicio = 0;
+        int minMediNoFin = 0;
+        int campanas = 0;
+        boolean validacionDatos;
+        boolean error;
+
+        //Pedimos al usuario que nos rellene las primeras 4 variables
+        /*
+        System.out.println("Ingrese la hora de inicio:");
+        horaInicio = scn.nextInt();
+        System.out.println("Ingrese los minutos de inicio:");
+        minutoInicio = scn.nextInt();
+        System.out.println("Ingrese la hora de fin:");
+        horaFin = scn.nextInt();
+        System.out.println("Ingrese los minutos de fin:");
+        minutoFin = scn.nextInt();
+
+        Esta parte es creada antes de realizar el bucle, para un mejor funcionamiento lo correcto es pedir y validar
+        datos dentro del bucle, por eso se comenta y se procede a meter dentro del bucle*/
+
+        //Controlamos las entradas para verificar que esten correctas
+
+        do {
+            //Valiamos hora de entrada
+            do {
+                System.out.println("Ingrese la hora de inicio:");
+                horaInicio = scn.nextInt();
+                error = (horaInicio < 0 || horaInicio > 23);
+                if (error) {
+                    System.out.println("Debe ingresar una hora de inicio valida. (0-23).");
+                }
+            } while (error);
+
+            //Validamos los minutos de entrada
+            do {
+                System.out.println("Ingrese los minutos de inicio:");
+                minutoInicio = scn.nextInt();
+                error = (minutoInicio < 0 || minutoInicio > 59);
+                if (error) {
+                    System.out.println("ERROR. Debe ingresar los minutos de inicio validos. (0-59).");
+                }
+            } while (error);
+
+            //Validamos hora de fin
+            do {
+                System.out.println("Ingrese la hora de fin:");
+                horaFin = scn.nextInt();
+                error = (horaFin < 0 || horaFin > 23);
+                if (error) {
+                    System.out.println("ERROR. Debe ingresar una hora de fin valida. (0-23).");
+                }
+            } while (error);
+
+            //Validamos minutos de fin
+            do {
+                System.out.println("Ingrese los minutos de fin:");
+                minutoFin = scn.nextInt();
+                error = (minutoFin < 0 || minutoFin > 59);
+                if (error) {
+                    System.out.println("ERROR. Debe ingresar los minutos de fin validos. (0-59).");
+                }
+            } while (error);
+
+            // Si todos los bucles se cumple bien, pasamos a comprobar que las horas pertenezcan al mismo dia
+            minMediNoInicio = (horaInicio * 60) + minutoInicio;
+            minMediNoFin = (horaFin * 60) + minutoFin;
+            validacionDatos = (minMediNoInicio >= minMediNoFin);
+            if (validacionDatos) {
+                System.out.println("ERROR. Los datos de fin deben ser mayores que los datos de inicio.");
+            }
+        } while (validacionDatos);
+
+        //Realizamos el cambio de las variables ingresadas a minutos para tener mayor facilidad de control
+        /*Este control tambien estaba en principio fuera del bucle, se procede a controlar dentro del bucle para
+        probar que las horas ingresadas pertenezcan al mismo dia
+
+        minMediNoInicio = (horaInicio * 60) + minutoInicio;
+        minMediNoFin = (horaFin * 60) + minutoFin;
+         */
+
+        //Esto nos permitetener un control y realizar un bucle sin problema
+
+        //Realizamos un bucle FOR para contar cuantas veces suena la campana
+        for (int i = minMediNoInicio; i <= minMediNoFin; i++) {
+            //Este bucle recoore cada minuto, incluyendo los minutos de inicio
+            //Necesitamos una variable dentro para saber en que minuto estamos
+
+            int multiplos = i % 60;
+
+            //Comprobamos si el minuto actual es una campanada (0,15,30,45)
+
+            if (multiplos % 15 == 0) {
+                campanas++;
+            }
+        }
+
+        //Mostramos el total de veces que ha sonado la campana
+        System.out.println("La campana suena un total de: " + campanas + " veces.");
+
+
+    }//Main
+}//Class
